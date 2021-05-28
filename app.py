5d14e4a89f03de4930db91a8722701d6a409a361
Almas Ansari <itsmealmas.ansari@gmail.com>
2021-05-28 22:28:46 +0530

    all fies added

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template,request,redirect  
+from cs50 import SQL
+    
+
+app=Flask(__name__)
+
+db=SQL("sqlite:///froshims.db")
+
+
+
+
+@app.route("/",methods=["POST","GET"])
+def index():
+    if request.method=="GET":
+        return render_template("main.html")
+    if request.method=="POST":
+        
+        x = request.form.get("name")
+        if x=="":
+            return "Name required"
+        y=request.form.get("sport") 
+        db.execute("INSERT INTO registrants (name,sport) VALUES(?,?)",x,y)
+        
+        return redirect("/done")
+
+@app.route("/done")
+def done():
+    l = db.execute("SELECT * FROM registrants ")
+    return render_template("added.html",l=l)
+
+
+
+
+
+
+        
